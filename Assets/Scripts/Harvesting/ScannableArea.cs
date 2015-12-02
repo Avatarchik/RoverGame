878234d2ebc7db0864fc6491df8720919c710d02
Taylor Hocutt <taylor.hocutt@gmail.com>
2015-12-01 21:43:18 -0500

    updated to unity 5.2.3, and began work on scannable/harvestable areas and menus

---
+++ Assets/Scripts/Harvesting/ScannableArea.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScannableArea : MonoBehaviour
+{
+    public float harvestTime;
+
+    public InventoryIngredient tier1Element;
+    public InventoryIngredient tier2Element;
+    public InventoryIngredient tier3Element;
+
+    public float rareElementHarvestChance;
+    public InventoryIngredient rareElement;
+
+    private bool scannable = true;
+    private Harvesting harvestingMenu;
+
+    public bool Scannable
+    {
+        get { return scannable; }
+        set
+        {
+            scannable = value;
+
+            //also something about showing an element on the screen?
+            harvestingMenu.Toggle();
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player") Scannable = true;
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player") Scannable = false;
+    }
+
+
+    private void Update()
+    {
+        if(Scannable && Input.GetKeyDown(KeyCode.Space))
+        {
+            //attempting to scan area open scanning menu!
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Crafting/Harvesting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Harvesting : MonoBehaviour
+{
+    public GameObject root;
+    public bool open;
+
+
+    public void Toggle()
+    {
+        open = !open;
+        root.SetActive(open);
+    }
+
+
+    private IEnumerator ScanTier()
+    {
+        yield return null;
+    }
+}
