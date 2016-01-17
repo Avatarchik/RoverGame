f1fbe4a32b52f5747c054fc63e448f15d774cd19
Taylor Hocutt <taylor.hocutt@gmail.com>
2016-01-16 19:27:03 -0500

    started fleshing out intro drop

---
+++ Assets/Scripts/UI/Base/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour
+{
+    public static FadeMenu FadeMenu;
+
+    public FadeMenu fadeMenu;
+	// Use this for initialization
+	void Start ()
+    {
+        FadeMenu = fadeMenu;
+	}
+}
