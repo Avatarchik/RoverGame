7fc00b947b02cd33446f54e6a94bd9ef78f99cd5
Taylor Hocutt <taylor.hocutt@gmail.com>
2016-02-03 10:10:19 -0500

    various changes to the main scene for the demo today

---
+++ Assets/Scripts/Logs/LogObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogObject : InteractibleObject
+{
+    public Log log;
+
+    public override void Interact()
+    {
+        if (Interactible)
+        {
+            LogMenu logMenu = UIManager.GetMenu<LogMenu>();
+            logMenu.AddLog(log);
+            logMenu.Open();
+        }
+    }
+}
