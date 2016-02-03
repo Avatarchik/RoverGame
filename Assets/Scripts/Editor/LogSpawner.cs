0bf168c7fa734a2e719d475b9e7a14900a882d4b
Taylor Hocutt <taylor.hocutt@gmail.com>
2016-02-02 19:30:53 -0500

    reimported usequencer, began fleshing out log entry data structure

---
+++ Assets/Scripts/Editor/LogSpawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class LogSpawner : MonoBehaviour
+{
+
+
+    [MenuItem("Assets/Create/Log")]
+    public static void CreateAsset()
+    {
+        ScriptableObjectUtility.CreateAsset<Log>();
+    }
+}
