11059812f7262c42bbb83e6dabb5b1f48e3f0697
Taylor Hocutt <taylor.hocutt@gmail.com>
2016-02-03 00:26:00 -0500

    added functionality for the log menu

--- Assets/Scripts/UI/Logs/LogMenu.cs
+++ Assets/Scripts/UI/Logs/LogMenu.cs
@@ -1,8 +1,93 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class LogMenu : Menu
 {
-    
+    public LogInfoPanel logInfoPanel;
+    public Transform logSlotContainer;
+    public Button closeButton;
+    public LogSlot logSlotPrefab;
+
+    public List<Log> logs = new List<Log>();
+    private List<LogSlot> logSlots = new List<LogSlot>();
+
+    private bool canClose = true;
+
+    public override void Open()
+    {
+        if (!isActive)
+        {
+            InitializeSlots(logs);
+            base.Open();
+        }
+    }
+
+
+    public override void Close()
+    {
+        if (isActive && canClose)
+        {
+            base.Close();
+        }
+    }
+
+
+    public void SelectLogSlot(LogSlot logSlot)
+    {
+        foreach (LogSlot ls in logSlots)
+        {
+            ls.IsSelected = false;
+        }
+
+        logSlot.IsSelected = true;
+        logInfoPanel.SelectedLog = logSlot.log;
+    }
+
+
+    private void InitializeSlots(List<Log> logList)
+    {
+        //TODO optomize this to reuse slots!
+        foreach (LogSlot cs in logSlots)
+        {
+            Destroy(cs.gameObject);
+        }
+        logSlots.Clear();
+
+        foreach (Log log in logs)
+        {
+            LogSlot newLogSlot = Instantiate(logSlotPrefab) as LogSlot;
+            newLogSlot.transform.SetParent(logSlotContainer);
+            newLogSlot.titleText.text = log.header;
+            newLogSlot.log = log;
+
+            logSlots.Add(newLogSlot);
+        }
+
+        if (logSlots.Count > 0) SelectLogSlot(logSlots[0]);
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            if(IsActive)
+            {
+                Close();
+            }
+            else
+            {
+                Open();
+            }
+        }
+    }
+
+
+    private void Awake()
+    {
+        LogSlot.OnSelectLogSlot += SelectLogSlot;
+        closeButton.onClick.AddListener(Close);
+    }
 }
---
+++ Assets/Scripts/UI/Logs/LogSlot.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LogSlot : MonoBehaviour
+{
+
+    public delegate void SelectLogSlot(LogSlot logSlot);
+    public static event SelectLogSlot OnSelectLogSlot;
+
+    public Button selectButton;
+
+    public Image mainImage;
+    public Text titleText;
+
+    public Color activeColor;
+    public Color inactiveColor;
+
+    public Log log;
+
+    private bool isSelected = false;
+
+    public bool IsSelected
+    {
+        get { return isSelected; }
+        set
+        {
+            isSelected = value;
+            if (isSelected)
+            {
+                mainImage.color = activeColor;
+            }
+            else
+            {
+                mainImage.color = inactiveColor;
+            }
+        }
+    }
+
+    private void SelectMe()
+    {
+        OnSelectLogSlot(this);
+    }
+
+    private void Awake()
+    {
+        selectButton.onClick.AddListener(SelectMe);
+    }
+}
