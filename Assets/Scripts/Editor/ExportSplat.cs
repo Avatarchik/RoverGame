e77ce4d5e43c6d61d4ff38bd8a4326fa143c7da6
Taylor Hocutt <taylor.hocutt@gmail.com>
2016-05-12 00:31:09 -0400

    optimized web scene. made new sparks effect

---
+++ Assets/Scripts/Editor/ExportSplat.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+
+public class ExportSplat : EditorWindow
+{
+    // For saving splat map as PNG file. 
+
+    [MenuItem("Terrain/Export Texture...")]
+    static void Apply()
+    {
+        Texture2D texture  = Selection.activeObject as Texture2D;
+        if (texture == null)
+        {
+            EditorUtility.DisplayDialog("Select Texture", "You Must Select a Texture first!", "Ok");
+            return;
+        }
+
+        var bytes = texture.EncodeToPNG();
+        File.WriteAllBytes(Application.dataPath + "/exported_texture.png", bytes);
+    }
+}
