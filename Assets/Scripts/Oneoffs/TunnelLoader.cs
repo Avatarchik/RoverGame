5d89e6c8f8d6be1dc677eb98ff91dc050f8c5e04
Taylor Hocutt <taylor.hocutt@gmail.com>
2016-05-17 19:54:33 -0400

    various change sto scripts, prefabs, and started a scene for making prefabs to dump into main.

---
+++ Assets/Scripts/Oneoffs/TunnelLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TunnelLoader : MonoBehaviour
+{
+    public int levelToLoad = 0;
+
+
+
+    void Start ()
+    {
+        SceneManager.LoadScene(levelToLoad, LoadSceneMode.Additive);
+	}
+}
