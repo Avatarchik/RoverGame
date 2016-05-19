43214b2ef396f7b2055484100001d31abfbe251e
Taylor Hocutt <taylor.hocutt@gmail.com>
2016-05-18 21:06:40 -0400

    preliminary work getting the questline set up for the web build

--- Assets/Scripts/Oneoffs/Intro.cs
+++ Assets/Scripts/Oneoffs/Intro.cs
@@ -38,16 +38,24 @@ namespace Sol
         public Ingredient wires;
         public Ingredient wheels;
 
+        public Ingredient canister;
+        public Ingredient fuelCell;
+        public Ingredient bundleOfWires;
+        public Ingredient explosive;
+
         public float displaySpeed = 0.01f;
 
         public Glitch glitchEffect;
 
         public bl_HudManager waypointManager;
 
+        public bool blowingLandslide = true;
+
         public List<Objective> introObjectives = new List<Objective>();
         public List<Transform> questWaypoints = new List<Transform>();
 
         private bool proceed = false;
+        
 
         public PlayerStats CachedPlayerStats
         {
@@ -76,13 +84,13 @@ namespace Sol
             sm.Play(START_MUSIC);
             sm.Play(WIND_EFFECT);
             ShowObjective(ot, "Yes! I got it activated!");
-			sm.Play (JENN_ACTIVATED);
+            sm.Play(JENN_ACTIVATED);
             StartCoroutine(GlitchOut());
             yield return new WaitForSeconds(delayTime);
             //
             //look around
             ShowObjective(ot, "Can you look around?");
-			sm.Play (JENN_LOOKAROUND);
+            sm.Play(JENN_LOOKAROUND);
 
             while (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0 || proceed)
             {
@@ -93,16 +101,16 @@ namespace Sol
             //
             //See that rover
             ShowObjective(ot, "Your wheels seem a bit damaged");
-			sm.Play (JENN_DAMAGEWHEELS);
+            sm.Play(JENN_DAMAGEWHEELS);
             yield return new WaitForSeconds(delayTime);
             ShowObjective(ot, "See that rover?");
-			sm.Play (JENN_SEEROVER);
+            sm.Play(JENN_SEEROVER);
             waypointManager.Huds[0].m_Target = questWaypoints[0];
             yield return new WaitForSeconds(delayTime);
             //
             //Take its wheels
             ShowObjective(ot, "Take and equip its wheels");
-			sm.Play (JENN_EQUIPWHEELS);
+            sm.Play(JENN_EQUIPWHEELS);
 
             while (inventory.GetIngredientAmount(wheels) == 0)
             {
@@ -116,7 +124,7 @@ namespace Sol
             //
             //Find the lift
             ShowObjective(ot, "Good, now find the lift");
-			sm.Play (JENN_FINDLIFT);
+            sm.Play(JENN_FINDLIFT);
             waypointManager.Huds[0].m_Target = questWaypoints[1];
             while (!proceed)
             {
@@ -127,12 +135,12 @@ namespace Sol
             //
             //Fix it
             ShowObjective(ot, "Console looks damaged.");
-			sm.Play (JENN_CONSOLEDAMAGED);
+            sm.Play(JENN_CONSOLEDAMAGED);
             yield return new WaitForSeconds(delayTime);
             //
             //get wires
             ShowObjective(ot, "You'll need wires to proceed. Try to scavenge them from that scrapped pod you passed.");
-			sm.Play (JENN_SCAVENGEWIRES);
+            sm.Play(JENN_SCAVENGEWIRES);
             waypointManager.Huds[0].m_Target = questWaypoints[2];
             yield return new WaitForSeconds(delayTime);
             while (inventory.GetIngredientAmount(wires) <= 6)
@@ -143,7 +151,7 @@ namespace Sol
             ///
             //fix it
             ShowObjective(ot, "Excellent, now fix the lift and ascend");
-			sm.Play (JENN_FIXLIFT);
+            sm.Play(JENN_FIXLIFT);
             waypointManager.Huds[0].m_Target = questWaypoints[1];
             while (!proceed)
             {
@@ -154,7 +162,7 @@ namespace Sol
             //
             //Compliment
             ShowObjective(ot, "That's a good rover");
-			sm.Play (JENN_GOODROVER);
+            sm.Play(JENN_GOODROVER);
             while (!proceed)
             {
                 yield return null;
@@ -167,8 +175,21 @@ namespace Sol
             yield return new WaitForSeconds(delayTime);
             ShowObjective(ot, "But I need to find out what happened to my friends on the planet.");
             yield return new WaitForSeconds(delayTime);
-            //
-            //go to the dome
+
+            StartCoroutine(RunShelfQuest());
+        }
+
+
+        private IEnumerator RunShelfQuest()
+        {
+            ObjectiveTracker ot = UIManager.GetMenu<ObjectiveTracker>();
+            Inventory inventory = UIManager.GetMenu<Inventory>();
+            FadeMenu fm = UIManager.GetMenu<FadeMenu>();
+            SoundManager sm = GameManager.Get<SoundManager>();
+            waypointManager.Huds[0].m_Target = null;
+
+            const float delayTime = 4f;
+
             ShowObjective(ot, "Looks like there was a landslide. I wonder if the base is ok.");
             waypointManager.Huds[0].m_Target = questWaypoints[3];
             while (!proceed)
@@ -179,55 +200,121 @@ namespace Sol
             yield return new WaitForSeconds(delayTime);
             //
             //get in
-            ShowObjective(ot, "Get inside");
-            while (!proceed)
-            {
-                yield return null;
-            }
-            yield return new WaitForSeconds(delayTime);
-            proceed = false;
-            //
-            //get in
-            sm.Play(BASE_MUSIC);
-            ShowObjective(ot, "Well Done!");
-            while (!proceed)
-            {
-                yield return null;
-            }
-            yield return new WaitForSeconds(delayTime);
-            proceed = false;
-            //
-            //get in
-            ShowObjective(ot, "Damn, the teleporter is busted...");
-            tunnelEnterObjective.SetActive(true);
+            ShowObjective(ot, "It doesn't look like there's any damage");
             yield return new WaitForSeconds(delayTime);
             //
-            //get into the caves
-            ShowObjective(ot, "Go back outside and look for a way into tunnels");
-            while (!proceed)
-            {
-                yield return null;
-            }
-            yield return new WaitForSeconds(delayTime);
-            proceed = false;
-            //
-            //you need explosives
-            ShowObjective(ot, "You'll need explosives to clear this landslide");
+            //strange
+            ShowObjective(ot, "Strange, the door is unlocked but it's jammed from the inside");
             yield return new WaitForSeconds(delayTime);
             //
-            //search
-            ShowObjective(ot, "Look around for a bundle of wires, a canister, and an active fuel cell to make a bomb");
+            //auxiliary entrance
+            ShowObjective(ot, "There should be a way into the base from the tunnels");
             yield return new WaitForSeconds(delayTime);
             //
-            //i need to go
-            ShowObjective(ot, "My boss is calling so I need to go. You're on your own for now");
+            //no good
+            ShowObjective(ot, "thats no good though with the entrance blocked...");
             yield return new WaitForSeconds(delayTime);
             //
-            //good luck
-            ShowObjective(ot, "Good luck little rover!");
+            //minute to think
+            ShowObjective(ot, "Standby, I need a minute to think");
+            yield return new WaitForSeconds(delayTime * 3);
+
+            //
+            //START AI
+            //human fool
+            ShowObjective(ot, "That human is a fool", false);
+            yield return new WaitForSeconds(delayTime);
+            //
+            //dont worry
+            ShowObjective(ot, "Don't worry, she can't hear us", false);
             yield return new WaitForSeconds(delayTime);
 
-            StartCoroutine(RunTunnels());
+            //
+            //HUMAN
+            //ok
+            ShowObjective(ot, "Ok I found an unstable energy reading nearby. I think its an old fuel cell");
+            yield return new WaitForSeconds(delayTime);
+            //
+            //make a bomb
+            ShowObjective(ot, "Get ahold of that and look around for anything else you need to make a bomb");
+            yield return new WaitForSeconds(delayTime);
+            waypointManager.Huds[0].m_Target = questWaypoints[4]; //canister
+            waypointManager.Huds[1].m_Target = questWaypoints[5]; //fuel cell
+            waypointManager.Huds[2].m_Target = questWaypoints[6]; //wires
+            //
+            //AI
+            //sarcasm
+            ShowObjective(ot, "Sure... Don't worry about the machine's safety", false);
+            yield return new WaitForSeconds(delayTime);
+
+            yield return new WaitForSeconds(delayTime);
+            while(inventory.GetIngredientAmount(canister) == 0 ||
+                inventory.GetIngredientAmount(canister) == 0 ||
+                inventory.GetIngredientAmount(canister) == 0)
+            {
+                if (inventory.GetIngredientAmount(canister) > 0) waypointManager.Huds[0].m_Target = null; //canister
+                if (inventory.GetIngredientAmount(fuelCell) > 0) waypointManager.Huds[1].m_Target = null; //fuel cell
+                if (inventory.GetIngredientAmount(wires) > 0) waypointManager.Huds[2].m_Target = null; //wires
+                yield return null;
+            }
+
+            //
+            //HUMAN
+            //make a bomb
+            ShowObjective(ot, "Good! You should be able to use those to craft a bomb at the nearby crafting station");
+            yield return new WaitForSeconds(delayTime);
+            while (inventory.GetIngredientAmount(explosive) == 0)
+            {
+                yield return null;
+            }
+
+            //
+            //there we go
+            ShowObjective(ot, "Excellent. Now wire it up to the landslide blocking the tunnels.");
+            waypointManager.Huds[0].m_Target = questWaypoints[7];
+            yield return new WaitForSeconds(delayTime);
+
+            //
+            //AI
+            //Don't listen
+            ShowObjective(ot, "Don't listen to the human, its dangerous in the tunnels", false);
+            yield return new WaitForSeconds(delayTime);
+            //
+            //AI
+            //Don't listen
+            ShowObjective(ot, "It would be much faster and easier to blow open the doors directly.", false);
+            waypointManager.Huds[1].m_Target = questWaypoints[8];
+            yield return new WaitForSeconds(delayTime);
+
+            StartCoroutine(Choice());
+        }
+
+
+        private IEnumerator Choice()
+        {
+            ObjectiveTracker ot = UIManager.GetMenu<ObjectiveTracker>();
+            Inventory inventory = UIManager.GetMenu<Inventory>();
+            FadeMenu fm = UIManager.GetMenu<FadeMenu>();
+            SoundManager sm = GameManager.Get<SoundManager>();
+            waypointManager.Huds[0].m_Target = null;
+
+            const float delayTime = 4f;
+            while(!proceed)
+            {
+                yield return null;
+            }
+            
+            if(blowingLandslide)
+            {
+                ShowObjective(ot, "I see how it is. Well lets just see how far trusting the human gets you", false);
+            }
+            else
+            {
+                ShowObjective(ot, "What are you doing?! You're going to damage the base!");
+            }
+            yield return new WaitForSeconds(delayTime);
+
+
         }
 
 
