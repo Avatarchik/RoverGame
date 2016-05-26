a71ca86cd760cd8d6a20737500dab31e46e31991
avanhale <avanhale@indiana.edu>
2016-05-25 21:11:13 -0400

    Cells dont rotate, back light  on click, puzzle scaling

--- Assets/Scripts/Oneoffs/PuzzleAnimTrigger.cs
+++ Assets/Scripts/Oneoffs/PuzzleAnimTrigger.cs
@@ -5,14 +5,29 @@ public class PuzzleAnimTrigger : MonoBehaviour {
 
 	Animator canvasAnim;
 	public bool enter;
+	[HideInInspector]
+	public bool fadeLight;
+	public Light puzzleLight;
+	private float currentTime;
+	private float lightFadeTime = 1.5f;
+	private float lightStartIntensity;
 
 	// Use this for initialization
 	void Start () {
 		canvasAnim = gameObject.GetComponent<Animator> ();
+		if (puzzleLight != null) {
+			lightStartIntensity = puzzleLight.intensity;
+			puzzleLight.intensity = 0.0f;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (puzzleLight != null && fadeLight && currentTime < lightFadeTime) {
+			currentTime += Time.unscaledDeltaTime;
+			float lerp = currentTime / lightFadeTime;
+			puzzleLight.intensity = Mathf.Lerp (0.0f, lightStartIntensity, lerp);
+		}
 	
 	}
 
