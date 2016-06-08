2b13d40f52505193c2151e2966751ba22f3a7893
Taylor Hocutt <taylor.hocutt@gmail.com>
2016-06-08 19:11:50 -0400

    implemented range finder

---
+++ Assets/Scripts/Oneoffs/RangeFinder.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RangeFinder : MonoBehaviour
+{
+    public Scrollbar scrollbar;
+
+    private bool scrolling = false;
+    private float target = Random.Range(0f, 1f);
+    private float current = 0f;
+    private float elapsedTime = 0f;
+    private float desiredTime = Random.Range(1f, 5f);
+
+    private void Update()
+    {
+        if(!scrolling)
+        {
+            scrolling = true;
+
+            current = scrollbar.value;
+            target = Random.Range(0f, 1f);
+            desiredTime = Random.Range(5f, 10f);
+            elapsedTime = 0f;
+        }
+        else
+        {
+            scrollbar.value = Mathf.Lerp(current, target, elapsedTime / desiredTime);
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime > desiredTime) scrolling = false;
+        }
+    }
+}
