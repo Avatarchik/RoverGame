bf52e578cf707213a14e720a2b9b1ddc2c8afa06
Raymond Stump <stump865@gmail.com>
2016-06-11 20:56:02 -0400

    Art Updates

---
+++ Assets/Scripts/Oneoffs/RandomRange.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRange : MonoBehaviour {
+
+    //Animator animator;
+    int randomnumber01 = 0;
+    public Animator WindTurbine01;
+    // Use this for initialization
+    void Start () {
+
+        WindTurbine01 = GetComponent<Animator>();
+        
+}
+	
+	// Update is called once per frame
+	void Update () {
+        randomnumber01 = Random.Range(1, 4);
+
+        WindTurbine01.SetInteger("randomnumber01", randomnumber01);
+    }
+}
