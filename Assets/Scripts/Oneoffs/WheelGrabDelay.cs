fa7244a1508db1dea67d3806b48c3eae293834dc
Alexander Van Halen <avanahale@indiana.edu>
2016-06-15 14:45:47 -0400

    Wheel Grab Delay

---
+++ Assets/Scripts/Oneoffs/WheelGrabDelay.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelGrabDelay : MonoBehaviour {
+
+	BoxCollider myGrabCollider;
+	public float grabDelay = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		myGrabCollider = gameObject.GetComponent<BoxCollider> ();
+		myGrabCollider.enabled = false;
+		StartCoroutine (ActivateDelay ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public IEnumerator ActivateDelay(){
+		yield return new WaitForSeconds (grabDelay);
+		myGrabCollider.enabled = true;
+	}
+}
